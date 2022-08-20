43a85a16be0f136e64354d5ec1711a25c7c91ff7
moringa <oscarokumu47@gmail.com>
2022-08-20 11:29:46 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // const age = 6;
 // const isAdult = age >= 18;
 // isAdult;
+
 function say(greeting, firstName) {
   return `${greeting} my name is ${firstName}`;
 }
@@ -84,4 +85,88 @@ console.log(names.slice(2, 3));
 
 let days = ['Mon','Tue','Wed','Thur','Fri']
 let day = days.slice(2 ,4);
-console.log(day);
+console.log(day); 
+
+let getAge = [33,34,56,12,45,35];
+function myAge(getAge){
+  for(let i = 0; i<getAge.length;i++){
+  console.log(`wrapped ${getAge[i]} and added a bow!`);
+  debugger;
+}
+return getAge;
+}
+myAge(getAge);
+
+let students = ['Irine','Lyz','Ivy','Eddwinna','Rehema'];
+function getStudents(students){
+  for(let i = 0;i<students.length;i++){
+    console.log(`I am ${students[i]} a student at moringa.`)
+  }
+  return students;
+}
+getStudents(students);
+let studentList =[ {
+  firstName: "Fransisca",
+  lastName: "Anyango",
+  favoriteAnimal: "Jaguar",
+}];
+
+const users = [
+  {
+    firstName: "Oscar",
+    lastName: "Morgan",
+    favoriteColor: "Blue",
+    favoriteAnimal: "Jaguar",
+    personalQuote: "You're never too old to learn something new,"
+  },
+  {
+    firstName: "Irene",
+    lastName: "Amoit",
+    favoriteColor: "Yellow",
+    favoriteAnimal: "Penguin",
+    personalQuote: "I just got lost in thought - it was unfamiliar"
+  },
+  {
+    firstName: "Kate",
+    lastName: "Travers",
+    favoriteColor: "Red",
+    favoriteAnimal: "Jaguar",
+    personalQuote: "Behind every great man is a a woman rolling her else"
+  },
+  {
+    firstName: "Drew",
+    lastName: "Price",
+    favoriteColor: "Yellow",
+    favoriteAnimal: "Elephant",
+    personalQuote:"Failure is not the opposite of success: it’s part of success",
+  }
+];
+
+function firstNamePrinter(collection){
+  for (const item of collection){
+    console.log(item.favoriteAnimal);
+  }
+} firstNamePrinter(users)
+
+function mineColor (collection) {
+  for (let kambona of collection) {
+    if (kambona.favoriteAnimal === 'Jaguar') {
+      console.log(kambona.firstName);
+    }
+  }
+} mineColor(users)
+ 
+function mineAnimal (collection) {
+  for(let okumu of collection){
+    if (okumu.favoriteColor === 'Yellow') {
+      console.log(okumu.lastName);
+    }
+  }
+}mineAnimal(studentList)
+function ofula (collection ,color) {
+  for(let user of collection ) {
+    if (user.favoriteColor === color){
+      console.log(user.firstName);
+    }
+  }
+} ofula(users,"Yellow")
